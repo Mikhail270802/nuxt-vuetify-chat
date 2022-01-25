8d4fec3933d6c90a24cdd08cce24b48b48a63202
Mikhail <misharygin@gmail.com>
2022-01-25 17:13:54 +0300

    initial

---
+++ pages/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="5000"
+      top
+      >
+      {{ message }}
+
+      <v-btn
+      color="blue"
+      flat
+      @click="snackbar = false"
+      >
+      Close
+      </v-btn>
+    </v-snackbar>
+
+        <v-card-title>
+          <h1>Nuxt Chat</h1>
+        </v-card-title>
+        <v-card-text>
+        <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="16"
+      :rules="nameRules"
+      label="Your Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="room"
+      :rules="roomRules"
+      label="Enter the room"
+      required
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="primary"
+      class="mr-4"
+      @click="submit"
+    >
+      Sign In
+    </v-btn>
+
+  </v-form>
+  </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+
+  export default {
+    layout: 'empty',
+    head: {
+      title: 'Simple Nuxt chat'
+    },
+    sockets: {
+      connect: function () {
+        console.log('socket connected')
+      }
+    },
+    data: () => ({
+      valid: true,
+      snackbar: false,
+      message: '',
+      name: '',
+      nameRules: [
+        v => !!v || 'Enter Name',
+        v => (v && v.length <= 16) || 'Name must be less than 16 characters',
+      ],
+      room: '',
+      roomRules: [v => !!v || 'Enter room']
+    }),
+
+    mounted() {
+      const {message} = this.$route.query
+      if(message === 'noUser'){
+        this.message = 'Enter data'
+      } else if(message === 'leftChat') {
+        this.message = 'You left the chat'
+      }
+
+      this.snackbar = !!this.message
+    },
+      
+    methods: {
+      ...mapMutations(["setUser"]),
+      submit () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            name: this.name,
+            room: this.room
+          };
+
+          this.$socket.emit('userJoined', user, data => {
+            if(typeof data === 'string') {
+              console.error(data)
+            } else {
+              user.id = data.userId 
+              this.setUser(user)
+              this.$router.push('/chat')
+            }
+          })
+        }
+      }
+    }
+  }
+
+</script>
